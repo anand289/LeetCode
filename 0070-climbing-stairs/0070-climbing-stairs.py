8802e9dfd7348672723317d5732e2a75a6356523
Anand Singh <43055217+anand289@users.noreply.github.com>
2025-03-17 13:58:23 +0530

    Time: 0 ms (100%), Space: 12.3 MB (98.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,23 +4,22 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # # Iterative
-        # dp = [0]*(n+1)
-        # dp[0] = 0
-        # dp[1] = 1
-        # if n > 1:
-        #     dp[2] = 2
-        # for i in range(3,n+1):
-        #     dp[i] = dp[i-1] + dp[i-2]
-        # return dp[n]
+        # Iterative
+        dp = [0]*(n+1)
+        dp[0] = 0
+        dp[1] = 1
+        if n > 1:
+            dp[2] = 2
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
 
-        # Recursive
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
-
+        # # Recursive : TLE
+        # if n == 1:
+        #     return 1
+        # if n == 2:
+        #     return 2
+        # return self.climbStairs(n-1) + self.climbStairs(n-2)
 
         # n = 5
         # idx:    0 1 2 3 4 5
